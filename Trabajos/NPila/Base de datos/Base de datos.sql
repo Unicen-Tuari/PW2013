7da344abce7412e6215e6f869121b93a9544a9c7
nahpl <nahuelmpila@gmail.com>
2013-06-22 00:36:58 -0300

    Cambios y carga de la base de datos

--- Trabajos/NPila/Base de datos/Base de datos.sql
+++ Trabajos/NPila/Base de datos/Base de datos.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Especialidad(
 	Id_especialidad TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	Nom_specialidad VARCHAR(15) NOT NULL,
+	Nom_especialidad VARCHAR(15) NOT NULL,
 	CONSTRAINT PK_ESPECIALIDAD PRIMARY KEY (Id_especialidad));
 
 CREATE TABLE Medico(
@@ -21,14 +21,14 @@ CREATE TABLE Paciente(
 	Localidad VARCHAR(20) NOT NULL,
 	Calle VARCHAR(50) NOT NULL,
 	Numero SMALLINT NOT NULL,
-	Telefono BIGINT NOT NULL,
-	Movil BIGINT,
+	Telefono NUMERIC(11) NOT NULL,
+	Movil NUMERIC(13),
 	Mail VARCHAR(30),
 	Fecha_nac DATE NOT NULL,
 	Obr_soc VARCHAR(40),
 	Num_afiliado VARCHAR(15),
 	Antecedentes TEXT,
-	Id_estudio TINYINT UNSIGNED NOT NULL,
+	Id_estudio TINYINT UNSIGNED,
 	Id_medico TINYINT UNSIGNED NOT NULL,
 	CONSTRAINT PK_PACIENTE PRIMARY KEY (DNI),
 	CONSTRAINT FK_PACIENTE_ESTUDIOS FOREIGN KEY (Id_estudio) REFERENCES Estudios(Id_estudio) ON DELETE CASCADE,
@@ -40,3 +40,23 @@ CREATE TABLE Turnos(
 	Hora TIME NOT NULL,
 	CONSTRAINT PK_TURNOS PRIMARY KEY (DNI),
 	CONSTRAINT FK_TURNOS_PACIENTE_DNI FOREIGN KEY (DNI) REFERENCES Paciente(DNI) ON DELETE CASCADE);
+
+INSERT INTO Especialidad (Nom_especialidad) VALUES
+	('Kinesiologia'),
+	('Nutricion');
+
+INSERT INTO Medico (Id_especialidad, Nombre_Apellido) VALUES
+	('1', 'Roberto Perez'),
+	('2', 'Juan Martinez');
+
+INSERT INTO Estudios (Estudio) VALUES
+	('nsjsabfksdbfksdbfkbsdfkbjdsfgsdfgdfs'),
+	('dsfsdflñheberbgoburegfb reufharbfuse');
+
+INSERT INTO Paciente VALUES
+	('35874586', 'Nahuel Pila', 'Las Flores', 'Bº Las Flores', '11', '454404', '2244422370', 'nahuelmpila@gmail.com', '19910904', 'ioma', '14047887/2', 'No presenta antecedentes', '1', '1'),
+	('35258745', 'Martin Pila', 'Las Flores', 'Bº Las Flores', '11', '450888', '2244912570', 'nahuelmpila_22@hotmail.com', '19990718', 'ioma', '14047887/19', 'No presenta antecedentes', '2', '2');
+
+INSERT INTO Turnos VALUES
+	('35874586', '20130809', '153000'),
+	('35258745', '20130706', '174000');
