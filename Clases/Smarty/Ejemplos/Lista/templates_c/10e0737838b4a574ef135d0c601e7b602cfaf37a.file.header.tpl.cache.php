265126f9982e56e6b484a35a2857ab6eb8e3a9c0
ignaciojonas <ignaciojonas@gmail.com>
2013-08-06 22:59:33 -0300

    Clase de Smarty + Orden en el repo

---
+++ Clases/Smarty/Ejemplos/Lista/templates_c/10e0737838b4a574ef135d0c601e7b602cfaf37a.file.header.tpl.cache.php
@@ -0,0 +1,28 @@
+<?php /* Smarty version Smarty-3.1.14, created on 2013-08-07 03:30:58
+         compiled from ".\templates\header.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:6299520181ad3e6a89-15906907%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '10e0737838b4a574ef135d0c601e7b602cfaf37a' => 
+    array (
+      0 => '.\\templates\\header.tpl',
+      1 => 1375833542,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '6299520181ad3e6a89-15906907',
+  'function' => 
+  array (
+  ),
+  'version' => 'Smarty-3.1.14',
+  'unifunc' => 'content_520181ad3efb22_99793316',
+  'has_nocache_code' => false,
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_520181ad3efb22_99793316')) {function content_520181ad3efb22_99793316($_smarty_tpl) {?><html>
+<head>
+	<title>Ejemplo Acceso al DOM</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body><?php }} ?>
